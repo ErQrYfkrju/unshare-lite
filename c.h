2eaefd6ef9cb1e44db5beca91dd3a4a8385c1bf2
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-11-14 22:34:29 +0200

    Add license header and comment where given function is taken from

--- c.h
+++ c.h
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Mark Vainomaa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #pragma once
 
 #ifndef _GNU_SOURCE
@@ -78,6 +102,7 @@ void __attribute__((__noreturn__)) exec_shell(void) {
     errexec(shell);
 }
 
+// Modified version of https://github.com/karelzak/util-linux/blob/08e3c9e6620232aa5433d102d3f8ac28bbae01b0/include/c.h#L175
 static inline int __attribute__((__noreturn__)) _do_err(int adderr, int code, const char *fmt, ...) {
     fprintf(stderr, "unshare: ");
     if(fmt != NULL) {
